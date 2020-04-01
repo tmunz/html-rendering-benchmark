66ce3cddad348aa196794a498b0d48448f8a1299
Tobias Munzert <tobias.munzert@gmail.com>
2020-04-01 23:44:23 +0200

    more benchmarks

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import React, { useState, useEffect } from 'react';
 export function App() {
 
   const [regularReact, setRegularReact] = useState(null);
+  const [stringReact, setStringReact] = useState(null);
 
   function createRegularReact() {
     let svg = null;
     let strategy = 1;
     const items = 5000;
 
-    console.time("regularReact create");
+    console.time("create");
     if (strategy === 0) {
       svg = <g>{new Array(items).fill(0).map((_, i) => <rect
         key={i}
@@ -31,23 +32,60 @@ export function App() {
       }
       // svg = <g>{svg}</g>;
     }
-    console.timeEnd("regularReact create");
-    console.time("regularReact render");
+    console.timeEnd("create");
+    console.time("render");
+    return svg;
+  }
+
+  function createStringReact() {
+    let svg = null;
+    let strategy = 0;
+    const items = 5000;
+
+    console.time("create");
+    if (strategy === 0) {
+      svg = `<g>${new Array(items).fill(0).map((_, i) => {
+        const size = Math.abs(400 - i * 2);
+        return `<rect
+          x="${i}" y="${i}"
+          width="${size}" height="${size}"
+          fill="blue" stroke="black" strokeWidth="1"
+          transform="rotate(${i} 200 200)"
+        />`;
+      })}</g>`;
+    }
+    console.timeEnd("create");
+    console.time("render");
     return svg;
   }
 
   useEffect(() => {
-    console.timeEnd("regularReact render");
+    console.timeEnd("render");
   })
 
   return (
     <div>
-      <h1>regular react</h1>
-      <button onClick={() => setRegularReact(null)}>clear</button>
-      <button onClick={() => setRegularReact(createRegularReact())}>create</button>
-      <svg width="400" height="400" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
-        {regularReact}
-      </svg>
+      <div>
+        <h1>regular react</h1>
+        <svg width="400" height="400" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
+          {regularReact}
+        </svg>
+        <div>
+          <button onClick={() => setRegularReact(null)}>clear</button>
+          <button onClick={() => setRegularReact(createRegularReact())}>create</button>
+        </div>
+      </div>
+      <div>
+        <h1>string react (dangerouslySetInnerHTML)</h1>
+        <svg width="400" height="400" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg"
+          dangerouslySetInnerHTML={{ __html: stringReact }}
+        >
+        </svg>
+        <div>
+          <button onClick={() => setStringReact(null)}>clear</button>
+          <button onClick={() => setStringReact(createStringReact())}>create</button>
+        </div>
+      </div>
     </div>
   );
 }
